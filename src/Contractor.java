26c7edc3f379cd9d1dab6cbefdaa931b15555a13
wndenis <winda.wzse@gmail.com>
2016-11-10 23:53:58 +0300

    Added "Divider" and "Contractor"

---
+++ src/Contractor.java
@@ -0,0 +1,12 @@
+public class Contractor extends Processor {
+
+    public Contractor() {
+        this.name = "Contractor";
+    }
+
+    @Override
+    public FInteger process(FInteger a, FInteger b) {
+        return a.contract(b);
+    }
+
+}
