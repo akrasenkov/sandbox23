00e34a8fad7405a21ea3ba744daa462dad14ebe7
Silencegod <gosha-95@bk.ru>
2016-11-11 03:20:09 +0300

    Added Divider and Contractor processes

---
+++ src/Contractor.java
@@ -0,0 +1,11 @@
+public class Contractor extends Processor {
+
+    public Contractor() {
+        this.name = "Contractor";
+    }
+
+    @Override
+    public FInteger process(FInteger a, FInteger b) {
+        return a.contract(b);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,14 @@ public class Main {
     public static Calculator calc;
 
     public static void main(String[] args) {
-        calc = new Calculator(
-                new Summator(),
-                new Amplifier()
-        );
-        System.out.println(calc.amp(
-                new FInteger(3),
-                new FInteger(4)
-        ).getValue());
-        System.out.println(calc.sum(
-                new FInteger(4),
-                new FInteger(12)
-        ).getValue());
+    Main main = new Main();
     }
 
+    public Main() {
+        calc = new Calculator(new Summator(), new Amplifier(), new Divider(), new Contractor());
+        System.out.println(calc.amp(new FInteger(3), new FInteger(4)).getValue());
+        System.out.println(calc.sum(new FInteger(4), new FInteger(12)).getValue());
+        System.out.println(calc.div(new FInteger(40), new FInteger(5)).getValue());
+        System.out.println(calc.cont(new FInteger(54), new FInteger(23)).getValue());
+    }
 }
