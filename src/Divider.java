9719ec83f676913395eac808fe9a790aefc17eac
Дмитрий Гайфулин < >
2016-11-12 16:49:56 +0300

    add Divider and Contractor

---
+++ src/Divider.java
@@ -0,0 +1,12 @@
+public class Divider extends Processor {
+
+    public Divider(){
+        this.name = "Divider";
+    }
+
+    @Override
+    public FInteger process(FInteger a, FInteger b){
+        return a.divide(b);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,12 @@ public class Main {
                 new Summator(),
                 new Amplifier()
         );
+
         System.out.println(calc.amp(
                 new FInteger(3),
                 new FInteger(4)
         ).getValue());
+
         System.out.println(calc.sum(
                 new FInteger(4),
                 new FInteger(12)
