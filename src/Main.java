224bac5377a44f8b3f1581f1060a061d15c9cf02
MrDmitriy666 <mr.dmitriy99@yandex.ru>
2016-11-12 18:00:48 +0300

    add div and com in Calculator

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,22 @@ public class Main {
     public static void main(String[] args) {
         calc = new Calculator(
                 new Summator(),
-                new Amplifier()
+                new Amplifier(),
+                new Divider(),
+                new Contractor()
         );
 
-        System.out.println(calc.amp(
-                new FInteger(3),
-                new FInteger(4)
-        ).getValue());
+        System.out.println(calc.amp(new FInteger(3),
+                new FInteger(4)).getValue());
 
-        System.out.println(calc.sum(
-                new FInteger(4),
-                new FInteger(12)
-        ).getValue());
+        System.out.println(calc.sum(new FInteger(4),
+                new FInteger(12)).getValue());
+
+        System.out.println(calc.div(new FInteger(100),
+                new FInteger(25)).getValue());
+
+        System.out.println(calc.con(new FInteger(20),
+                new FInteger(6)).getValue());
     }
 
 }
