a037b4b61f6e7415944639d57713ba302ff386a1
Brainbuggged <brainbuggged@gmail.com>
2016-11-12 21:59:57 +0300

    New Processors were added(Divider and Contractor) Calculator was changed Main main = new main()... git commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,10 +2,14 @@ public class Calculator {
 
     private Processor amplifier;
     private Processor summator;
+    private Processor divider;
+    private Processor contractor;
 
-    public Calculator(Processor amplifier, Processor summator) {
-        this.amplifier = amplifier;
+    public Calculator(Processor summator, Processor contractor, Processor amplifier, Processor divider) {
         this.summator = summator;
+        this.amplifier = amplifier;
+        this.divider=divider;
+        this.contractor=contractor;
     }
 
     public FInteger sum(FInteger a, FInteger b) {
@@ -14,5 +18,11 @@ public class Calculator {
 
     public FInteger amp(FInteger a, FInteger b) {
         return amplifier.process(a, b);
+    }
+    public FInteger div(FInteger a, FInteger b){
+        return divider.process(a,b);
+    }
+    public FInteger con(FInteger a , FInteger b){
+        return contractor.process(a,b);
     }
 }
