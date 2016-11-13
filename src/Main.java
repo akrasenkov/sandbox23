f6f358895b33dd98507ea544036eda2e18e84def
AlinaOskina <alicemirror1211@gmail.com>
2016-11-13 17:38:46 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
     public Main(){
         Calculator calc;
 
-        calc = new Calculator(new Summator(), new Amplifier(), new Divider(), new Contractor());
-        System.out.println(calc.sum(new FInteger(4),new FInteger(12)).getValue());
-        System.out.println(calc.cont(new FInteger(7),new FInteger(5)).getValue());
-        System.out.println(calc.amp(new FInteger(3),new FInteger(4)).getValue());
-        System.out.println(calc.div(new FInteger(40),new FInteger(10)).getValue());
+        calc = new Calculator(new Summator(), new Contractor(), new Amplifier(), new Divider());
+        System.out.println(calc.sum(new FInteger(21),new FInteger(2)).getValue());
+        System.out.println(calc.cont(new FInteger(100),new FInteger(5)).getValue());
+        System.out.println(calc.amp(new FInteger(4),new FInteger(7)).getValue());
+        System.out.println(calc.div(new FInteger(400),new FInteger(4)).getValue());
     }
     public static void main(String[] args) {
         Main main = new Main();
