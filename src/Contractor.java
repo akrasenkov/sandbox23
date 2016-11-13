6dd53df7829d87764a8eb28fa5a53576419d52c8
AlinaOskina <alicemirror1211@gmail.com>
2016-11-13 17:32:23 +0300

    Update Contractor.java

--- src/Contractor.java
+++ src/Contractor.java
@@ -1,1 +1,13 @@
+public class Contractor extends Processor{
 
+    public  Contractor()
+    {
+        this.name="Contractor";
+    }
+
+    @Override
+    public FInteger process( FInteger c, FInteger d)
+    {
+        return c.cont(d);
+    }
+}
