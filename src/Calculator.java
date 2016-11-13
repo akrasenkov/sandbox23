9f7d44ccc2c9e343cbc9770a102bfb468767f829
AlinaOskina <alicemirror1211@gmail.com>
2016-11-13 17:26:12 +0300

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,15 @@
 public class Calculator {
 
-    private Processor amplifier;
-    private Processor summator;
+    protected Processor amplifier;
+    protected Processor summator;
+    protected Processor divider;
+    protected Processor contractor;
 
-    public Calculator(Processor amplifier, Processor summator) {
+    public Calculator(Processor amplifier, Processor summator, Processor divider, Processor contractor) {
         this.amplifier = amplifier;
         this.summator = summator;
+        this.divider = divider;
+        this.contractor = contractor;
     }
 
     public FInteger sum(FInteger a, FInteger b) {
@@ -14,5 +18,13 @@ public class Calculator {
 
     public FInteger amp(FInteger a, FInteger b) {
         return amplifier.process(a, b);
+    }
+
+    public  FInteger div(FInteger a, FInteger b){
+        return  divider.process(a,b);
+    }
+
+    public  FInteger cont(FInteger a, FInteger b){
+        return contractor.process(a,b);
     }
 }
