3c588e2042a927fba631d20ebc84c249d31ea47e
minly4114 <ms.liza.semenova.1998@mail.ru>
2016-11-13 19:58:53 +0300

    fixed divider

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,13 +3,13 @@ public class Calculator {
     private Processor amplifier;
     private Processor summator;
     private Processor contractor;
-    private Processor devider;
+    private Processor divider;
 
-    public Calculator(Processor amplifier, Processor summator, Processor contractor, Processor devider) {
+    public Calculator(Processor amplifier, Processor summator, Processor contractor, Processor divider) {
         this.amplifier = amplifier;
         this.summator = summator;
         this.contractor = contractor;
-        this.devider = devider;
+        this.divider = divider;
     }
 
     public FInteger sum(FInteger a, FInteger b) {
@@ -22,5 +22,5 @@ public class Calculator {
 
     public FInteger contract(FInteger a, FInteger b) {return  contractor.process(a, b);}
 
-    public FInteger divide(FInteger a, FInteger b) {return devider.process(a, b);}
+    public FInteger divide(FInteger a, FInteger b) {return divider.process(a, b);}
 }
