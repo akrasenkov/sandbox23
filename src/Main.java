cf9d2d63d1b4322fcc042c9e95109e1d31083089
AlinaOskina <alicemirror1211@gmail.com>
2016-11-13 17:29:15 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,15 @@
 public class Main {
 
-    public static Calculator calc;
+    public Main(){
+        Calculator calc;
 
-    public static void main(String[] args) {
-        calc = new Calculator(
-                new Summator(),
-                new Amplifier()
-        );
-        System.out.println(calc.amp(
-                new FInteger(3),
-                new FInteger(4)
-        ).getValue());
-        System.out.println(calc.sum(
-                new FInteger(4),
-                new FInteger(12)
-        ).getValue());
+        calc = new Calculator(new Summator(), new Amplifier(), new Divider(), new Contractor());
+        System.out.println(calc.sum(new FInteger(4),new FInteger(12)).getValue());
+        System.out.println(calc.cont(new FInteger(7),new FInteger(5)).getValue());
+        System.out.println(calc.amp(new FInteger(3),new FInteger(4)).getValue());
+        System.out.println(calc.div(new FInteger(40),new FInteger(10)).getValue());
     }
-
+    public static void main(String[] args) {
+        Main main = new Main();
+    }
 }
