48d8ae44793bbfdccd294eb71d7517a71e2b2bc6
Eugene <chuggachuggachu@gmail.com>
2016-11-14 16:03:37 +0300

    try

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
-    public static Calculator calc;
-
-    public static void main(String[] args) {
-        calc = new Calculator(
+    public void init() {
+        Calculator calc = new Calculator(
                 new Summator(),
-                new Amplifier()
+                new Contractor(),
+                new Amplifier(),
+                new Divider()
         );
         System.out.println(calc.amp(
                 new FInteger(3),
@@ -15,6 +15,19 @@ public class Main {
                 new FInteger(4),
                 new FInteger(12)
         ).getValue());
+        System.out.println(calc.con(
+                new FInteger(10),
+                new FInteger(3)
+        ).getValue());
+        System.out.println(calc.div(
+                new FInteger(20),
+                new FInteger(5)
+        ).getValue());
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.init();
     }
 
 }
