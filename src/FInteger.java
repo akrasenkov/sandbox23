bfefff32e04bf9a91bb9dd2741d6d9d3662285e4
Yuriy Sergeev <ususus_russ@mail.ru>
2016-11-16 00:17:47 +0400

    Added two operations: division and substraction, which are work using Processor

--- src/FInteger.java
+++ src/FInteger.java
@@ -10,17 +10,14 @@ public class FInteger {
         return new FInteger(value + b.getValue());
     }
 
-    public FInteger contract(FInteger b) {
-        return new FInteger(value - b.getValue());
-    }
-
-    public FInteger divide(FInteger b) {
-        return new FInteger(value / b.getValue());
-    }
-
     public FInteger amplify(FInteger b) {
         return new FInteger(value * b.getValue());
     }
+
+    public FInteger contract(FInteger b) {return new FInteger(value - b.getValue());}
+
+    public FInteger div(FInteger b) {return new FInteger(value / b.getValue());}
+
     public int getValue() {
         return value;
     }
