cbc87c8f731f21a76e18bf31595cc3da90ad99e3
Pavel Spirin <ramsay33@mail.ru>
2016-11-16 02:25:04 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         calc = new Calculator(
+                new Amplifier(),
                 new Summator(),
-                new Amplifier()
+                new Divider(),
+                new Contracter()
         );
         System.out.println(calc.amp(
                 new FInteger(3),
@@ -13,6 +15,14 @@ public class Main {
         ).getValue());
         System.out.println(calc.sum(
                 new FInteger(4),
+                new FInteger(12)
+        ).getValue());
+        System.out.println(calc.div(
+                new FInteger(36),
+                new FInteger(9)
+        ).getValue());
+        System.out.println(calc.con(
+                new FInteger(40),
                 new FInteger(12)
         ).getValue());
     }
